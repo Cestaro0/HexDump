8cb8434e261646667190f059a0ea0f637e021aa4
Cestaro0 <luizfelipecestaro2005@gmail.com>
2023-01-17 11:38:13 -0300

    v1.1

--- HexDump/hxd.cpp
+++ HexDump/hxd.cpp
@@ -1,13 +1,13 @@
 #include "hxd.h"
 
-auto hxd::Init(int NumberOfArguments, char* FileRawData) -> void
+auto hxd::Init(int NumberOfArguments, char* FilePath) -> void
 {
 	if (NumberOfArguments < 2)
 	{
-		printf("Erro: Considere usar o exemplo: HDump.exe Arquivo.exe\n");
+		printf("Erro: Considere usar o exemplo: HexDump.exe Arquivo.exe\n");
 	}
 
-	std::ifstream File(FileRawData, std::ios::binary);
+	std::ifstream File(FilePath, std::ios::binary);
 
 	if (File.fail())
 	{
@@ -15,56 +15,14 @@ auto hxd::Init(int NumberOfArguments, char* FileRawData) -> void
 	}
 
 	int SizeOfFile = GetFileSize(File);
-	char* BinaryFile = reinterpret_cast<char*>(malloc(SizeOfFile * sizeof(char)));
+	char* FileRawData = reinterpret_cast<char*>(malloc(SizeOfFile * sizeof(char)));
 
-	if (!File.read(BinaryFile, SizeOfFile))
+	if (!File.read(FileRawData, SizeOfFile))
 	{
 		printf("Erro: Nao foi possivel ler os dados\n");
 	}
 
-	DumpHex(BinaryFile, SizeOfFile);
-}
-
-auto hxd::DumpHex(const void* FileRawData, size_t SizeOfFile) -> void
-{
-	char AsciiVisualization[17];
-	AsciiVisualization[16] = '\0';
-
-	for (int i = 0; i < SizeOfFile; ++i)
-	{
-		printf("%02X ", ((unsigned char*)FileRawData)[i]);
-
-		if (((unsigned char*)FileRawData)[i] >= ' ' && ((unsigned char*)FileRawData)[i] <= '~')
-		{
-			AsciiVisualization[i % 16] = ((unsigned char*)FileRawData)[i];
-		}
-		else
-		{
-			AsciiVisualization[i % 16] = '.';
-		}
-
-		if ((i + 1) % 8 == 0 || (i + 1) == SizeOfFile)
-		{
-			printf(" ");
-			if ((i + 1) % 16 == 0)
-			{
-				printf("|  %s \n", AsciiVisualization);
-			}
-			else if (i + 1 == SizeOfFile)
-			{
-				AsciiVisualization[(i + 1) % 16] = '\0';
-				if ((i + 1) % 16 <= 8)
-				{
-					printf(" ");
-				}
-				for (int j = (i + 1) % 16; j < 16; ++j)
-				{
-					printf("   ");
-				}
-				printf("|  %s \n", AsciiVisualization);
-			}
-		}
-	}
+	DumpHex(FileRawData, SizeOfFile);
 }
 
 auto hxd::GetFileSize(std::ifstream& File) -> int
@@ -76,4 +34,117 @@ auto hxd::GetFileSize(std::ifstream& File) -> int
 	File.seekg(0, std::ios::beg);
 
 	return sizeFile;
+}
+
+auto hxd::DumpHex(const void* FileRawData, int SizeOfFile) -> void
+{
+	char AsciiVisualization[17];
+	AsciiVisualization[16] = '\0';
+
+	int offset = 0;
+
+	for (int i = 0; i < SizeOfFile; i++)
+	{
+		offset = i + 1;
+
+		if (i == 0)
+		{
+			WriteHeader();	//gambiarra :P
+			printf("%08X  |  ", i);
+		}
+
+		printf("%02X ", ((unsigned char*)FileRawData)[i]);
+
+		AsciiVisualization[i % 16] = GetAsciiVisualization(((unsigned char*)FileRawData)[i]);
+
+		if (offset % 8 == 0)
+		{
+			printf(SPACE);
+		}
+
+		if (offset % 16 == 0)
+		{
+			printf("|  %s \n", AsciiVisualization);
+
+			if (offset < SizeOfFile)
+			{
+				printf("%08X  |  ", offset);
+			}
+		}
+	}
+}
+
+/*auto hxd::HexDumpLine(const void* FileRawData, int size) -> void
+{
+	char AsciiVisualization[17] = { 0 };
+	AsciiVisualization[16] = NULL_TERMINATOR;
+
+	int offset = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		offset = i + 1;
+
+		if (i == 0)
+		{
+			WriteHeader();	//gambiarra :P
+			printf("%08X  |  ", i);
+		}
+
+		printf("%02X ", ((unsigned char*)FileRawData)[i]);
+
+		AsciiVisualization[i % 16] = GetAsciiVisualization(((unsigned char*)FileRawData)[i]);
+		
+		if (offset % 8 == 0)
+		{
+			printf(SPACE);
+		}
+
+		if (offset % 16 == 0)
+		{
+			printf("|  %s \n", AsciiVisualization);
+			
+			if (offset < size)
+			{
+				printf("%08X  |  ", offset);
+			}
+		}
+	}
+} -> implementar futuramente*/
+
+
+auto hxd::WriteHeader() -> void			
+{
+	printf("\nOffset h");
+	printf("     ");
+
+	for (int i = 0; i < 16; i++)
+	{
+		printf("%02X ", i);
+		if ((i + 1) % 8 == 0)
+		{
+			printf(SPACE);
+		}
+	}
+
+	printf("   ");
+	
+	for (int i = 0; i < 16; i++)
+	{
+		printf("%X", i);
+	}
+
+	printf("\n\n");
+}
+
+auto hxd::GetAsciiVisualization(unsigned char FileRawData) -> char
+{
+	char HexToAscii = NULL_CHARACTER;
+
+	if (((unsigned char)FileRawData) >= MIN_CHARACTER && ((unsigned char)FileRawData) <= MAX_CHARACTER)
+	{
+		HexToAscii = ((unsigned char)FileRawData);
+	}
+
+	return HexToAscii;
 }
--- HexDump/hxd.h
+++ HexDump/hxd.h
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <fstream>
 
+#define MIN_CHARACTER ' '
+#define MAX_CHARACTER '~'
+#define NULL_CHARACTER '.'
+#define SPACE " "
+#define NULL_TERMINATOR '\0'
+
 class hxd
 {
 public:
@@ -9,6 +15,9 @@ public:
 
 private:
 	auto GetFileSize(std::ifstream& File) -> int;
-	auto DumpHex(const void* FileRawData, size_t SizeOfFile) -> void;
+	auto DumpHex(const void* FileRawData, int SizeOfFile) -> void;
+	//auto HexDumpLine(const void* FileRawData, int size) -> void; -> implementar futuramente
+	auto WriteHeader() -> void;
+	auto GetAsciiVisualization(unsigned char FileRawData) -> char;
 };
 
